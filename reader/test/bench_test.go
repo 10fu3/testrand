7483616d292f73add1732148644617352bec8faa
10fu3 <msf.1192@gmail.com>
2023-11-19 22:00:20 +0900

    add benchmark

---
+++ reader/test/bench_test.go
@@ -0,0 +1,40 @@
+package test
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+	"testrand/reader/eval"
+)
+
+func BenchmarkRead(b *testing.B) {
+	sample := strings.NewReader(`
+	(begin
+	  (define local-word-count (new-hashmap))
+	  (foreach-array (string-split (read-file "sample1.txt") " ")
+	    (lambda (word)
+	      (put-hashmap local-word-count word (+ (get-hashmap local-word-count word 0) 1))
+	    )
+	  )
+	)
+	`)
+	read := eval.New(bufio.NewReader(sample))
+	result, err := read.Read()
+	if err != nil {
+		panic(err)
+	}
+	env, err := eval.NewGlobalEnvironment()
+	ctx := context.Background()
+	if err != nil {
+		panic(err)
+	}
+	b.StartTimer()
+	result, err = eval.Eval(ctx, result, env)
+	fmt.Println(result)
+	b.StopTimer()
+	if err != nil {
+		panic(err)
+	}
+}
