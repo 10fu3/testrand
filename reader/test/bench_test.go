d770522ffe8912b95cb36655e1aa07d4414f6c3f
10fu3 <msf.1192@gmail.com>
2023-12-01 00:52:38 +0900

    add bench

--- reader/test/bench_test.go
+++ reader/test/bench_test.go
@@ -42,3 +42,31 @@ func BenchmarkRead(b *testing.B) {
 		panic(err)
 	}
 }
+
+func BenchmarkLoop(b *testing.B) {
+	sample := strings.NewReader(`
+(begin
+(define a 0)
+(loop (< a 10000) (begin
+(set a (+ a 1))
+))
+)
+	`)
+	read := eval.New(bufio.NewReader(sample))
+	result, err := read.Read()
+	if err != nil {
+		panic(err)
+	}
+	env, err := eval.NewGlobalEnvironment()
+	ctx := context.Background()
+	if err != nil {
+		panic(err)
+	}
+	b.StartTimer()
+	result, err = eval.Eval(ctx, result, env)
+	b.StopTimer()
+	fmt.Println(result)
+	if err != nil {
+		panic(err)
+	}
+}
